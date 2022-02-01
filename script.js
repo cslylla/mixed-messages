24dab3a9a8861a27f6cdfc26f0ea5ae74336b93f
cslylla <81328857+cslylla@users.noreply.github.com>
2022-02-01 11:51:47 +0100

    Generate messages

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
-function generateRandomNumber() {
-    return Math.floor(Math.random()*3);
+function generateRandomNumber(num) {
+    return Math.floor(Math.random()*num);
+};
+
+const library = {
+   subject: ["you","your life", "life"],
+   verb: ["are", "is"],
+   object: ["miserable", "average", "worthless", "painful"],
+};
+
+let quote = [];
+
+quote.push(library["subject"][generateRandomNumber(3)]);
+
+if(quote[0]=== "you"){
+   quote.push(library["verb"][0]);
+} else {
+    quote.push(library["verb"][1]);
 }
+   quote.push(library["object"][generateRandomNumber(4)]); 
 
-let quote = ["You are average.", "Life is difficult.", "You cannot do it."];
-
-console.log(quote[generateRandomNumber()]);
+console.log(quote);
